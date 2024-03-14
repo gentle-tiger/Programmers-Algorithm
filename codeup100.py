4090f8d35ebd19172e09dabcf7880b7633169257
Choi Hojin <jkhh9725@gmail.com>
2024-03-14 22:55:50 +0900

    codeup-100

--- codeup100.py
+++ codeup100.py
@@ -321,55 +321,203 @@ print(int(a) <= int(b))
 a,b = input().split()
 print(int(a) != int(b))
 
-# 52번
+# 52번 정수 입력받아 참 거짓 평가하기
+n = int(input())
+print(bool(n))
 
-# 53번
+# 53번 참 거짓 바꾸기
+n = int(input())
+print(not bool(n))
 
-# 54번
+# 54번 둘 다 참일 경우만 참 출력하기
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(bool(a) and bool(b))
+# 55번 하나라도 참이면 참 출력하기
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(bool(a) or bool(b))
 
-# 55번
+# 56번 참/거짓이 서로 다를 때에만 참 출력하기
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(bool(a) != bool(b))
 
-# 56번
+# 57번 참/거짓이 서로 같을 때에만 참 출력하기
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(bool(a) == bool(b))
 
-# 57번
+# 58번 둘 다 거짓일 경우만 참 출력하기
+a,b = input().split()
+a = int(a)
+b = int(b)
+print( not bool(a) and not bool(b) ) # and는 모두 True일 때 True, not은 논리값을 뒤집습니다.
 
-# 58번
+# 59번 비트단위로 NOT 하여 출력하기
+a = input()
+a = int(a) 
+print(~a) # ~1 = -2
 
-# 59번
+# 60번 비트단위로 AND 하여 출력하기
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a & b) # 3 & 5 = 1 
 
-# 60번
+# 61번 비트단위로 OR 하여 출력하기
+a,b = input().split()
+a = int(a)
+b = int(b)  
+print(a | b) # 3 | 5 = 7
 
-# 61번
+# 62번 비트단위로 XOR 하여 출력하기
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a ^ b) # 3 ^ 5 = 6
 
-# 62번
-
-# 63번
+# 63번 정수 2개 입력받아 큰 값 출력하기
+a, b = input().split()
+a = int(a)
+b = int(b)
+a if a > b else b
 
 # 64번
+# input().split()으로 입력된 값을 공백을 기준으로 분할한 후 
+# map()함수를 사용하여 각 요소를 int로 변환하고 map의 결과를 다시 풀어서 각 변수에 할당합니다. 
+a,b,c = map(int, input().split()) 
+print(min(a,b,c))
 
-# 65번
+# 65번 정수 3개 입력받아 짝수만 출력하기 "1 2 3 4 5 6"
+# 1
+numbers = list(map(int ,input().split())) # [1, 2, 3, 4, 5, 6] 
+result = filter(lambda x : x % 2 == 0 ,numbers)      
+for num in result : 
+    print(num)
+# 2
+a,b,c = input().split() 
+a = int(a)
+b = int(b)
+c = int(c)
+if a % 2 == 0 : print(a) 
+if b % 2 == 0 : print(b) 
+if c % 2 == 0 : print(c) 
 
-# 66번
+# 66번 정수 3개 입력받아 짝/홀 출력하기
+# 1
+numbers = list(map(int, input().split()))
+for num in numbers  :
+    print("even") if num % 2 == 0 else print("odd")
+# 2
+a,b,c = map(int,input().split())
+print("even") if a % 2 == 0 else print("odd")
+print("even") if b % 2 == 0 else print("odd")
+print("even") if c % 2 == 0 else print("odd")
 
-# 67번
+# 67번 정수 1개 입력받아 분류하기
+n = int(input())
+if n < 0 :
+    print("A") if n % 2 == 0 else print("B")
+else :
+    print("C") if n % 2 == 0 else print("D")
 
-# 68번
+# 68번 점수 입력받아 평가 출력하기
+# 1
+n = int(input())
+if n >= 90 :
+    print("A")
+elif 70 <= n < 90 :
+    print("B")
+elif 40 <= n < 70 :
+    print("C")
+elif 0 <= n < 40 :
+    print("D")
+else :
+    print("")
+    
+# 2 
+n = int(input())
+grade = "A" if n >= 90 else "B" if n >= 70 else "C" if n >= 40 else "D"
+print(grade) if grade else None   
 
-# 69번
+ 
+# 69번 평가 입력받아 다르게 출력하기
+s = input()
+grade = "best!!!" if s == "A" else "good!!" if s == "B" else "run!" if s == "C" else "slowly~" if s == "D" else "what?"
+print(grade)
 
-# 70번
+# 70번 월 입력받아 계절 출력하기
 
-# 71번
+    
+s = int(input())
+if (s == 12 or s ==  1 or s ==  2) : 
+    print("winter")
+elif (s == 3 or s ==  4 or s ==  5) :
+    print("spring")
+elif (s == 6 or s ==  7 or s ==  8) :
+    print("summer")
+else  :
+    print("fall")
+    
+# 2
+n = int(input())
+if n // 3 == 1 : 
+    print("spring")
+elif n // 3 == 2 :
+    print("summer")
+elif n // 3 == 3 :
+    print("fall")
+else : print("winter") 
 
-# 72번
 
-# 73번
+# 71번 0 입력될 때까지 무한 출력하기
+n = 1
+while n != 0 :  # n이 0이 아니면 반복문은 계속 실행되며 해당 숫자가 출력된다. 만약 0이라면 반복문은 종료된다.
+    n = int(input())
+    if n != 0 :
+        print(n)  # 입력된 정수를 줄을 바꿔 하나씩 출력하는데, 0이 입력되면 종료한다. (0은 출력하지 않는다.)
+# 72번 정수 1개 입력받아 카운트다운 출력하기1
+# 1
+n = int(input())
+while n != 0 :
+    print(n)
+    n = n - 1
+# 2 
+n = int(input())
+while True :
+    print(n)
+    n = n - 1
+    if(a == 0) : 
+        break
 
-# 74번
 
-# 75번
+# 73번 정수 1개 입력받아 카운트다운 출력하기2
+n = int(input())
 
-# 76번
+while n != 0 :
+    n = n - 1 # 위치만 변경
+    print(n)
+
+# 74번 문자 1개 입력받아 알파벳 출력하기
+s = ord(input())
+t = ord('a')
+
+while t <= s:
+    print(chr(t), end="") # end=' '는 마지막에 줄을 바꾸지 않고 빈칸만 띄운다.
+    t += 1                # end='\n'로 작성하거나 생략하면, 값을 출력한 후 마지막(end)에 줄바꿈(newline)이 된다.
+
+# 75번 문자 1개 입력받아 알파벳 출력하기
+s = ord(input())
+t = ord("a")
+while t <= s :
+    print(chr(t))
+    t += 1
+# 76번 정수 1개 입력받아 그 수까지 출력하기1
 
 # 77번
 
