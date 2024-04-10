67b81c20bbc9a946936020a5bb6acc04c1a7af4d
Choi ho jin <jkhh9725@gmail.com>
2024-04-10 22:09:34 +0900

    DFS,BFS

--- CodingTest-DFS&BFS.py
+++ CodingTest-DFS&BFS.py
@@ -176,5 +176,126 @@ dfs(graph, 1, visited) #### 시작 노드가 1이라는 뜻.
 
 
 
+# -----------------------
+# 음료수 얼려먹기 
+
+# DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문 
+
+def dfs(x,y) :
+    # 주어진 범위를 벗어지는 경우에는 즉시 종료 
+    if x <= -1 or x >= n or y <= -1 or y >= m :
+        return False
+    
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0 :
+        # 해당 노드 방문 처리 
+        graph[x][y] = 1
+        # 상하좌우의 위치들도 모두 재귀적으로 호출
+        dfs(x - 1, y)  # 왼쪽
+        dfs(x + 1, y)  # 오른쪽
+        dfs(x, y - 1)  # 아래
+        dfs(x, y + 1)  # 위
+        return True  # 현재 노드가 음료수인 경우 True 반환. 만약 여기서 x or y 가 범위를 넘어가더라도 해당 값이 다시 dfs를 실행할 때 조건식에서 걸려 false를 반환하게 된다. 
+    return False  # 현재 노드가 이미 방문한 경우 False 반환
+
+
+
+n,m = map(int,'4 5'.split()) 
+print(f'n :{n}, m :{m}')
+s = list('00110 00011 11111 00000'.split())
+s
+graph = []
+#### 2차원 배열 만들기 
+for i in s :
+    row = []
+    for j in i :
+        row.append(int(j))
+    graph.append(row)
+    
+#### 2차원 배열 출력해보기
+for i in range(n) : 
+    print(graph[i])
+
+graph[1][1]
+
+# 모든 노드(위치)에 대하여 음료수 채우기 
+result = 0
+for i in range(n) :
+    for j in range(m) :
+        # 현재 위치에서 DFS 수행
+        if dfs(i,j) == True :
+            result += 1
+            
+print(result)
+
+
+# -----------------
+# 미로 탈출
+from collections import deque
+
+# BFS 소스코드 구현 
+def bfs(x,y) : 
+    # queue 구현을 위해 deque 라이브러리 사용
+    queue = deque()
+    queue.append((x,y))
+    print('queue :', queue)
+    # 큐가 빌 때까지 반복하기 
+    while queue :
+        x, y = queue.popleft()
+        # 현재 위치에서 4가지 방향으로의 위치 확인 
+        for i in range(4) :
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or nx > n or ny < 0 or ny > m :
+                continue
+            
+            # 괴물이 있는 부분 무시  
+            if graph[nx][ny] == 0 :
+                continue
+            
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록 
+            if graph[nx][ny] == 1 :
+                graph[nx][ny] == graph[x][y] + 1
+                queue.append((nx,ny))
+    # 가장 오른쪽 아래까지의 최단 거리 반환 
+    return graph[n-1][m-1]
+
+                
+
+
+
+
+
+
+# N, M을 공백기준으로 구분하여 입력 받기 
+# n, m = map(int, input().split())
+n, m = map(int, '4 5'.split())
+print(f'n :{n},  m :{m}')
+
+# 2차원 리스트의 맵 정보 입력 받기 
+test = list('101010 111111 000001 111111 111111'.split())
+test
+
+graph = []
+
+# for i in range(n) :
+#     graph.append(list(map(int,input())))
+print('graph :', graph)
+for i in test :
+    arr = []
+    for j in i : 
+        arr.append(int(j))
+    graph.append(arr)
+
+print('graph :', graph) 
+
+# 이동할 네 가지 방향 정의(상, 하, 좌, 우)
+dx = [-1, 1, 0, 0] # (-1,0) 상 ~ (0,1) 우
+dy = [0, 0, -1, 1]
+
+# BFS를 수행한 결과 출력
+print(bfs(1,0))
+
 
 
